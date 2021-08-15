efc218da9e4c500aee8357ec25f86b69137d5c70
onethps <onethps@gmail.com>
2021-08-15 10:00:31 +0300

    added react-icon library

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
+import {AiOutlineClose, GoThreeBars} from "react-icons/all";
 
 function Header() {
+
+    const[sidebar, setSidebar] = useState(false)
+
+    const showSidebar = () => setSidebar(!sidebar)
+
     return (
         <div>
-<NavLink to='/pre-junior' >  Pre-Junior</NavLink>
-<NavLink to='/junior' >   junior</NavLink>
-<NavLink to='/junior+' >   junior+</NavLink>
+            <GoThreeBars onClick={showSidebar}/>
 
+<nav className={sidebar ? 'nav-menu active' : 'nav-menu'}/>
+            <ul className='nav-menu-items'>
+                <li className='navbar-toggle'>
+                    <AiOutlineClose/>
+                    <NavLink to='/pre-junior' className='menu-bars'> Pre-Junior</NavLink>
+                    <NavLink to='/junior'> junior</NavLink>
+                    <NavLink to='/junior+'> junior+</NavLink>
+                </li>
+
+            </ul>
         </div>
     )
 }
